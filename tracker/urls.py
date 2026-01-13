2144a5ae0af494c03ba772981ac505030433bfe7
tatyana kuleva <tatianaarsentievna@gmail.com>
2026-01-13 15:22:09 +0300

    add pagination, tests

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from tracker.apps import TrackerConfig
-from .views import HabitListCreateAPIView, HabitRetrieveUpdateDestroyAPIView
+from .views import HabitCreateAPIView, HabitListAPIView, PublicHabitListAPIView,  HabitRetrieveUpdateDestroyAPIView
 
 app_name = TrackerConfig.name
 
 urlpatterns = [
-    path('habits/', HabitListCreateAPIView.as_view(), name='habit-list-create'),
-    path('habits/<int:pk>/', HabitRetrieveUpdateDestroyAPIView.as_view(), name='habit-detail'),
+    path('habits/', HabitListAPIView.as_view(), name='habit-list'),
+    path('habits/public/', PublicHabitListAPIView.as_view(), name='habit-public-list'),
+    path('habits/create/', HabitCreateAPIView.as_view(), name='habit-create'),
+    path('habits/<int:pk>/', HabitRetrieveUpdateDestroyAPIView.as_view(), name='habit-detail')
 ]
